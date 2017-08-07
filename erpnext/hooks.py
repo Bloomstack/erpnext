7c5c8a8b8ffc09c2b577c8cf3ca713fe0ec00604
Valmik Jangla <valmik2k2@gmail.com>
2017-08-07 15:37:16 +0530

    removed custom footer, customer not mandatory in wc

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -201,12 +201,6 @@ scheduler_events = {
 
 email_brand_image = "assets/erpnext/images/erpnext-logo.jpg"
 
-default_mail_footer = """<div style="text-align: center;">
-	<a href="https://erpnext.com?source=via_email_footer" target="_blank" style="color: #8d99a6;">
-		Sent via ERPNext
-	</a>
-</div>"""
-
 get_translated_dict = {
 	("doctype", "Global Defaults"): "frappe.geo.country_info.get_translated_dict"
 }
