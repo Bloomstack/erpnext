86a22d45b4a94bed2301f192b5f441ffd0844d55
Rohan <rohan@digithinkit.com>
2018-04-17 16:50:28 +0530

    Add logic for status updater

--- erpnext/crm/doctype/contract/contract.py
+++ erpnext/crm/doctype/contract/contract.py
@@ -3,8 +3,26 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+
 from frappe.model.document import Document
+from frappe.utils import get_datetime, nowdate
+
 
 class Contract(Document):
-	pass
+	def validate(self):
+		self.update_status()
+
+	def on_update_after_submit(self):
+		self.update_status()
+
+	def update_status(self):
+		now_date = get_datetime(nowdate())
+		start_date = get_datetime(self.start_date)
+		end_date = get_datetime(self.end_date)
+
+		if self.is_signed:
+			status = "Active" if start_date < now_date < end_date else "Inactive"
+		else:
+			status = "Unsigned"
+
+		self.status = status
