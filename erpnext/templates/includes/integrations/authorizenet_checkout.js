e9411966a9af91ca2db84bb0587dbb5e468b0e17
Rohan Bansal <rohan@digithinkit.com>
2020-04-24 17:04:36 +0530

    format: checkout page

--- erpnext/templates/includes/integrations/authorizenet_checkout.js
+++ erpnext/templates/includes/integrations/authorizenet_checkout.js
@@ -1,11 +1,13 @@
-$('#submit').on("click", function (e) {
-    $('#submit').prop('disabled', true)
-	$('#submit').html(__('Processing...'))
-    data = context.replace(/'/g, '"');
+$('#submit').on("click", function(e) {
+    $('#submit').prop('disabled', true);
+    $('#submit').html(__('Processing...'));
+
+    let data = context.replace(/'/g, '"');
     e.preventDefault();
-    cardNumber = document.getElementById('card-number').value;
-    expirationDate = document.getElementById('card-expiry').value;
-    cardCode = document.getElementById('card-code').value;
+
+    let cardNumber = document.getElementById('card-number').value;
+    let expirationDate = document.getElementById('card-expiry').value;
+    let cardCode = document.getElementById('card-code').value;
 
     frappe.call({
         method: "erpnext.erpnext_integrations.doctype.authorizenet_settings.authorizenet_settings.charge_credit_card",
@@ -16,12 +18,11 @@ $('#submit').on("click", function (e) {
             "card_code": cardCode,
             "data": data
         },
-        
-        callback: function (r) {
+
+        callback: function(r) {
             if (r.message.status === "Completed") {
-                window.location.href = "/integrations/payment-success"
-            }
-            else {
+                window.location.href = "/integrations/payment-success";
+            } else {
                 frappe.throw(__(`${r.message.description}`));
             }
         }
