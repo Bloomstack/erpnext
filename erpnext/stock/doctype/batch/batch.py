fd11e1694cb2e0cb39c8a82ab6da58db3517d393
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2020-08-06 17:36:54 +0530

    Added an endpoint to provide the user with a downlodable coa link

--- erpnext/stock/doctype/batch/batch.py
+++ erpnext/stock/doctype/batch/batch.py
@@ -326,7 +326,11 @@ def save_thc_cbd(batch_no, thc, cbd):
 	doc.save()
 
 @frappe.whitelist()
-def get_active_batch(item_code):
-	active_batch = frappe.get_all("Batch", {"item": item_code, "display_on_website": 1})
+def get_active_batch(item_code, coa=False):
+	if(coa):
+		fields = ["name", "certificate_of_analysis"]
+	else:
+		fields = ["name"]
+	active_batch = frappe.get_all("Batch", filters={"item": item_code, "display_on_website": 1},  fields=fields )
 	active_batch = active_batch[0] if active_batch else {}
 	return active_batch
