30b30179d422a544230d065e66bd2566c4bb2df8
nmimsnikhil <nikhilbachhawat.nmims@gmail.com>
2020-08-20 17:47:47 +0530

    fix: changes done as commented

--- erpnext/patches/v13_0/create_investor_opportunity_type.py
+++ erpnext/patches/v13_0/create_investor_opportunity_type.py
@@ -1,10 +1,8 @@
 from __future__ import unicode_literals
 import frappe
 def execute():
-	names = frappe.db.get_list('Opportunity Type','name')
-	for item in names:
-		if item.get("name") == "Investor":
-			return
+	if frappe.db.exists("Opportunity Type", "Investor"):
+		return
 	doc = frappe.new_doc("Opportunity Type")
 	doc.update({
 		"name": "Investor"
