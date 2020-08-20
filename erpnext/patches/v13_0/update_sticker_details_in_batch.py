1a4a4dfdf56932d8e81014590986b07432bef111
bhattdevarsh <58166671+bhattdevarsh@users.noreply.github.com>
2020-08-20 17:03:25 +0530

    Update erpnext/patches/v13_0/update_sticker_details_in_batch.py
    Co-authored-by: Rohan <Alchez@users.noreply.github.com>

--- erpnext/patches/v13_0/update_sticker_details_in_batch.py
+++ erpnext/patches/v13_0/update_sticker_details_in_batch.py
@@ -4,7 +4,7 @@ from erpnext.stock.doctype.batch.batch import update_batch_doc
 
 def execute():
 	frappe.reload_doc("stock", "doctype", "quality_inspection")
-	quality_inspection = frappe.get_all("Quality Inspection", fields=["name", "item_code", "batch_no", "thc", "cbd"])
+	quality_inspection = frappe.get_all("Quality Inspection", filters={"docstatus": 1}, fields=["name", "item_code", "batch_no", "thc", "cbd"])
 	for qi in quality_inspection:
 		if qi.thc or qi.cbd:
 			update_batch_doc(qi.batch_no, qi.name, qi.item_code)
