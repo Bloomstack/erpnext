f2326edf746a8785787b00e8a0af7364321b7a6b
nmimsnikhil <nikhilbachhawat.nmims@gmail.com>
2020-08-24 19:39:08 +0530

    fix: checking for all sales order in each picklist and set the minimum among the all sales order item

--- erpnext/patches/v13_0/set_values_in_pick_list.py
+++ erpnext/patches/v13_0/set_values_in_pick_list.py
@@ -6,10 +6,13 @@ def execute():
 	}, fields= ["name","customer"])
 	for pick_list in pick_lists:
 		order_delivery_date = []
-		sales_order = frappe.get_value("Pick List Item",{"parent": pick_list.name}, "sales_order")
-		if sales_order:
-			delivery_dates = frappe.get_all("Sales Order Item", {"parent": sales_order}, "delivery_date")
-			for delivery in delivery_dates:
-				order_delivery_date.append(delivery.delivery_date)
+		sales_orders = frappe.get_all("Pick List Item", filters={"parent": pick_list.name}, fields=["sales_order"], distinct=True)
+		# sales_order = frappe.get_value("Pick List Item",{"parent": pick_list.name}, "sales_order")
+		for sales_order in sales_orders:
+			if sales_order.sales_order:
+				delivery_dates = frappe.get_all("Sales Order Item", {"parent": sales_order.sales_order}, "delivery_date")
+				for delivery in delivery_dates:
+					order_delivery_date.append(delivery.delivery_date)
+		if order_delivery_date:
 			frappe.db.set_value("Pick List", pick_list.name, "delivery_date", min(order_delivery_date), update_modified=False)
-			frappe.db.set_value("Pick List", pick_list.name, "customer_name", pick_list.customer, update_modified=False)
+		frappe.db.set_value("Pick List", pick_list.name, "customer_name", pick_list.customer, update_modified=False)
