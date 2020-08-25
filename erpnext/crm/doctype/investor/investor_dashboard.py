608f6f5f2b7aab4387fd606220de8bc7dbbbe234
vishdha <vishaldhayagude07@gmail.com>
2020-08-25 22:01:01 +0530

    fix:investor dashboard fixed

--- erpnext/crm/doctype/investor/investor_dashboard.py
+++ erpnext/crm/doctype/investor/investor_dashboard.py
@@ -3,13 +3,16 @@ from frappe import _
 
 def get_data():
 	return {
-		'fieldname': 'investor_name',
+		'heatmap': True,
+		'heatmap_message': _('This is based on the transaction of this Investor'),
+		'fieldname': 'name',
 		'non_standard_fieldnames': {
-			'Lead': 'party_name',
-			'Opportunity': 'party_name'
+			'Lead': 'lead_name',
+			'Opportunity': 'customer_name'
 		},
 		'transactions': [
 			{
+				'label': _('Transactions'),
 				'items': ['Lead', 'Opportunity']
 			}
 		]
