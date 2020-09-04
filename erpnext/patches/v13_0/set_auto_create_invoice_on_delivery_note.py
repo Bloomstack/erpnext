d8546c3257204728d3e42dd1e659aa40d19ae063
Myuddin khatri <myuddin@digithinkit.com>
2020-09-04 16:28:20 +0530

    fixed patch

--- erpnext/patches/v13_0/set_auto_create_invoice_on_delivery_note.py
+++ erpnext/patches/v13_0/set_auto_create_invoice_on_delivery_note.py
@@ -3,6 +3,7 @@ import frappe
 def execute():
 	frappe.reload_doc("accounts", "doctype", "accounts_settings")
 
-	create_invoice_on_submit = frappe.db.get_single_value("Accounts Settings", "auto_create_invoice_on_delivery_note_submit")
-	if create_invoice_on_submit:
-		frappe.db.set_value("Accounts Settings", None, "auto_create_invoice_on_delivery_note", "Submit")
+	if frappe.get_meta("Accounts Settings").has_field("auto_create_invoice_on_delivery_note_submit"):
+		create_invoice_on_submit = frappe.db.get_single_value("Accounts Settings", "auto_create_invoice_on_delivery_note_submit")
+		if create_invoice_on_submit:
+			frappe.db.set_value("Accounts Settings", None, "auto_create_invoice_on_delivery_note", "Submit")
