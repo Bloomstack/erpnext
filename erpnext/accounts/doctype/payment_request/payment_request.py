20ad2ced122f4cba41307df9d2f6a529bbf15be9
rvpasari <rvpasari@gmail.com>
2020-11-17 19:03:40 +0530

    feat: creating a payment request against a quotation

--- erpnext/accounts/doctype/payment_request/payment_request.py
+++ erpnext/accounts/doctype/payment_request/payment_request.py
@@ -177,8 +177,11 @@ class PaymentRequest(Document):
 
 		#create a sales order if the payment request has been made against a quotation
 		if self.reference_doctype == "Quotation":
+			#we submit the quotation
+			ref_doc.submit()
+			frappe.db.commit()
 			from erpnext.selling.doctype.quotation.quotation import _make_sales_order
-			sales_order = frappe.get_doc(_make_sales_order(self.reference_name, ignore_permissions=True))
+			sales_order = frappe.get_doc(_make_sales_order(ref_doc.get("name"), ignore_permissions=True))
 			#reference document is the new sales order we just created
 			payment_entry = get_payment_entry("Sales Order", sales_order.name,
 				party_amount=party_amount, bank_account=self.payment_account, bank_amount=bank_amount)
