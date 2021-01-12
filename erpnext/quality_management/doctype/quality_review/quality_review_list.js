3ccf36cc7a2e3cbb058e7433ed47497eeaf95f7e
hitenshKharva <kharva.hitensh11@hotmail.com>
2021-01-12 13:04:51 +0000

    fix: status filter triggering error in Quality review

--- erpnext/quality_management/doctype/quality_review/quality_review_list.js
+++ erpnext/quality_management/doctype/quality_review/quality_review_list.js
@@ -1,12 +1,15 @@
 frappe.listview_settings['Quality Review'] = {
-	add_fields: ["action"],
+	add_fields: ["action", "status"],
 	get_indicator: function(doc)
 	{
 		if(doc.action == "No Action") {
 			return [__("No Action"), "green", "action,=,No Action"];
-		}
-		else if(doc.action == "Action Initialised") {
+		} else if (doc.action == "Action Initialised") {
 			return [__("Action Initialised"), "red", "action,=,Action Initialised"];
+		} else if (doc.status === "Open") {
+			return [__("Open"), "red", "status,=,Open"];
+		} else if (doc.status === "Closed") {
+			return [__("Closed"), "green", "status,=,Closed"];
 		}
 	}
 };
