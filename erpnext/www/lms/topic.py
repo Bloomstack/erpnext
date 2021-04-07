760861db60e55a3e5c9fac15c7e8443cd0623625
sahil28297 <37302950+sahil28297@users.noreply.github.com>
2021-04-07 16:07:02 +0530

    Update erpnext/www/lms/topic.py

--- erpnext/www/lms/topic.py
+++ erpnext/www/lms/topic.py
@@ -66,7 +66,6 @@ def calculate_contents_progress(contents):
 def get_ongoing_topic(contents):
 	ongoing_content = None
 	ongoing_topics = [content for content in contents if not content.get("completed")]
-	if len(contents) != len(ongoing_topics) and ongoing_topics[0]:
+	if len(contents) != len(ongoing_topics) and len(ongoing_topics) > 0:
 		ongoing_content = ongoing_topics[0].get('content')
 	return ongoing_content
-
